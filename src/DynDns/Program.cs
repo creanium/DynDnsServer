71a00191cca84e1060689e4ad1943f4fba2fbe2d
creanium <john@creanium.net>
2025-01-23 14:32:38 -0700

    Add Serilog

--- src/DynDns/Program.cs
+++ src/DynDns/Program.cs
@@ -1,5 +1,15 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Add support to logging with Serilog
+builder.Host.UseSerilog((context, configuration) =>
+	configuration.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
+
+//Add support to logging request with SERILOG
+app.UseSerilogRequestLogging();
 
 app.MapGet("/", () => "Hello World!");
 
